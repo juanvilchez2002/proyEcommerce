25b6de6408c3024f52aebb4425a7faae3d257856
justincanales <justin_jeriko@hotmail.com>
2021-11-08 18:13:32 -0500

    footer en proceso

---
+++ ecommerce/src/componentes/footer.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function footer() {
+    return (
+        <div>
+          <footer className="text-black py-4 bg-dark">
+             <div className='container'>
+                 <nav className="row">
+                 <ul className='col-12 col-md-3 list-unstyled'>
+                <li className='font-weigth-bold mb-4'>Siguenos</li>
+                <li className='d-flex justify-content-between'>
+                  <i className="bi bi-facebook"/>
+                  <i className="bi bi-instagram"/>
+                  <i className="bi bi-twitter"/>
+                </li>
+                 </ul> 
+
+             </nav>
+         
+           </div>
+      
+       </footer> 
+    
+     </div>
+    )
+}
+
+
+
