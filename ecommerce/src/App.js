a303f5adef6e988811a2c71074599dd389140234
Juan A. Vilchez Huarcaya <juanvilchez2002@gmail.com>
2021-11-11 18:03:05 -0500

    mostrar producto - inicio

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,13 +1,17 @@
 import Cards from "./components/Cards";
 import "bootstrap/dist/css/bootstrap.min.css";//importando libreria de BootStrap
 import "./css/styles.css"
+import Producto from "./components/Producto";
 
 function App() {
   return (
-      <div className="card-inicio">
-        <Cards/>
-      </div>
-  );
+      
+
+        <div className="d-flex align-items-center justify-content-center">
+          <Producto/>
+        </div>
+      
+  )
 }
 
 export default App;
