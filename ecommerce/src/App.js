837fa6eb75e550611ed728e03edb73bd485bf694
CristoperYarleque <caypsaturno@gmail.com>
2021-11-18 20:41:55 -0500

    cambios beta

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,13 +1,43 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.min.js"
 import "bootstrap-icons/font/bootstrap-icons.css"
-import Header from "./components/Header/Header"
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
+import Inicio from "./components/inicio/Inicio"
+import Tienda from "./components/tienda/Tienda"
+import Noticias from "./components/noticias/Noticias"
+import AcercaDe from "./components/acercade/AcercaDe"
+import Logearse from "./components/logeo/Logearse"
+import { AuthContextProvider } from "./context/authContext"
+import PrivateRoute from "./components/logeo/PrivateRoute"
 
 export default function App() {
+
+
   return (
-    <div>
-      <Header />
-    </div>
+  <AuthContextProvider>
+    <Router>
+        <Routes>
+          <Route path="/inicio" element={
+            <PrivateRoute>
+              <Inicio />
+            </PrivateRoute>} />
+          <Route path="/" element={<Logearse/>}/>
+          <Route path="/tienda" element={
+          <PrivateRoute>
+          <Tienda/>
+          </PrivateRoute>  } />
+          <Route path="/noticias" element={
+          <PrivateRoute>
+            <Noticias />
+            </PrivateRoute>} />
+          <Route path="/acercade" element={
+          <PrivateRoute>
+            <AcercaDe />
+            </PrivateRoute>} />
+        </Routes>
+    </Router>
+  </AuthContextProvider>
+      
   )
 }
 
